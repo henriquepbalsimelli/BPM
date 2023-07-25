d1750a4c3031c151fdf26f815cae75c4554ce13f
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-07-24 22:52:53 -0300

    fas

--- Pages/test.js
+++ Pages/test.js
@@ -2,7 +2,7 @@
 export default function Test() {
     return(
         <>
-            <div>Págeina de teste</div>
+            <div>Página de teste</div>
             <div>Saporra não funciona?</div>
         </>
     )
