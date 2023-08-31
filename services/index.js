a306f8e13f157f75b7a9513e753c878d5b42bca7
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-08-31 19:46:03 -0300

    mockando dados da api

--- services/index.js
+++ services/index.js
@@ -2,6 +2,20 @@
 const API_URL = process.env.API_URL
 
 export default async function getData () {
-    return await fetch(`${API_URL}/data`)
-        .then((res) => res.json())
+    // return await fetch(`${API_URL}/data`)
+    //     .then((res) => res.json())
+    return {
+        data: [
+            {
+                id: 1,
+                title: 'Post 1',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl. Donec euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl.',
+            },
+            {
+                id: 2,
+                title: 'Post 2',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl. Donec euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc eu nisl.',
+            },
+        ]
+    }
 }
