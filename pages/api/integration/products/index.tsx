b541b5c3e36abba67e438c732913c2754e51dd13
henriquePedrozo <henrique.pedrozo@shopper.com.br>
2023-10-30 13:46:36 -0300

    fix: arrumando retorno da rota integration/product

--- pages/api/integration/products/index.tsx
+++ pages/api/integration/products/index.tsx
@@ -7,14 +7,10 @@ export default async function handler(req: any, res: any) {
     const products = await productService.getProductByFamily('4806871524')
 
     const valuesA: any = []
-
-    products.forEach((value: any)=>{
-        valuesA.push(value)
-    })
   
     res.status(200).json({
         name: 'John Doe',
-        products: valuesA
+        products: products
     })
   
 }
